23008c8cffaad00377aa1579b6de955f96c4263d
vplacido <valentinplacido25@gmail.com>
2020-02-19 14:53:39 -0500

    started lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -9,5 +10,24 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do 
+    
+      title = params[:title]
+      content = params[:content]
+      @article = Article.create(title: title, content: content)
+      #  binding.pry
+      redirect '/articles/#{article.id}'
+  end
+
+  get '/articles/:id' do
+    id = params[:id]
+    @article = Article.find(id)
+    erb :show
   end
 end
